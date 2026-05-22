6a6799fd13364695bdf669ee12ef29668ddeb745
Somebud0180 <ethanlagera@icloud.com>
2026-05-22 14:32:13 +0800

    Cleaned up AccessibilityPermission

--- ConType/AccessibilityPermission.swift
+++ ConType/AccessibilityPermission.swift
@@ -4,24 +4,10 @@
 //
 //  Created by Ethan John Lagera on 4/8/26.
 //
-//  Code referenced from AXorcist
-//  https://github.com/steipete/AXorcist/
+//  Code referenced from Hold To Talk
+//  https://github.com/jxucoder/hold-to-talk/tree/main
 
-import ApplicationServices
 import CoreGraphics
-
-//public enum AccessibilityPermission {
-//    @MainActor
-//    public static func isTrusted() -> Bool {
-//        AXIsProcessTrusted()
-//    }
-//
-//    @MainActor
-//    static func requestPrompt() -> Bool {
-//        let options = [kAXTrustedCheckOptionPrompt.takeUnretainedValue() as String: true]
-//        return AXIsProcessTrustedWithOptions(options as CFDictionary?)
-//    }
-//}
 
 /// Enum for managing Input Monitoring (TCC) permissions on macOS.
 /// Uses CoreGraphics APIs to check and request permission.
